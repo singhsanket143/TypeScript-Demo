53beb5ed2613052369665c984badf15d733607dd
Sanket Singh <singhsanket143@gmail.com>
2023-10-02 20:55:05 +0530

    Create 2_creating_alias.ts

---
+++ 2_creating_alias.ts
@@ -0,0 +1,14 @@
+type Details = {name: string, marks: number, address? : string};
+// Details is kind of like an alias or a nickname to {name: string, marks: number, address? : string}
+
+const result1 : Details = {
+    name: "Sanket",
+    marks: 98
+}
+
+
+const result2 : Details = {
+    name: "Sarthak",
+    marks: 95
+}
+
